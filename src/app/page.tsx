be4f8d999c8a11188928183e84a8a4a675783762
Magico <keming_wang@outlook.com>
2025-05-27 10:49:24 -0400

    fix favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
             priority
           />          
       </WiggleElement>
-      <main className="flex flex-col gap-[32px] row-start-2 items-start lg:items-center ">
+      <main className="flex flex-col gap-[32px] row-start-2 items-start md:items-center ">
         
         <p className="text-black text-6xl">
           A-ripple of your imagination.
@@ -22,7 +22,7 @@ export default function Home() {
      
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
-            className="rounded-full border border-solid border-black transition-colors flex items-center justify-center  text-black gap-2 hover:bg-[#73AB84] hover:text-white font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            className="rounded-full border border-solid border-black transition-colors flex items-center justify-center text-black gap-2 hover:bg-[#73AB84] hover:text-white font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             
             rel="noopener noreferrer"
           >
